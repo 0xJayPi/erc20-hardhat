78549c294fee2bd74dcd51d4ccf2bd487539efbf
0xJayPi <jpcampaya@gmail.com>
2023-01-19 20:22:39 +0100

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+module.exports = {
+    defaultNetwork: "localhost",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+            url: "http://127.0.0.1:8545/",
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 5,
+            blockConfirmations: 6,
+            gas: 2100000,
+            gasPrice: 8000000000,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.17" }],
+    },
+    mocha: {
+        timeout: 2000000,
+    },
+}
