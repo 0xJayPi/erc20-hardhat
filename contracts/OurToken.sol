7931e6b627670306fb6c4101aaaaf6a53c22352b
0xJayPi <jpcampaya@gmail.com>
2023-01-20 01:00:45 +0100

    NatSpec added

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @author JP Campaya
+/// @title Another ERC20 token
 contract OurToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("JP Token", "JPC") {
         _mint(msg.sender, initialSupply);
+    }
+
+    /// Mint tokens
+    /// @param amount the number of tokens to mint
+    /// @dev calls the ERC20's _mint function, which also transfer the minted tokens to the msg.sender
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
     }
 }
