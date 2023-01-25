593391b55fd14749ea55b4d36c5f22189438b857
0xJayPi <jpcampaya@gmail.com>
2023-01-25 22:55:11 +0100

    Ownable added to contract

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @author JP Campaya
 /// @title Another ERC20 token
-contract OurToken is ERC20 {
+contract OurToken is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("JP Token", "JPC") {
         _mint(msg.sender, initialSupply);
     }
@@ -13,7 +14,7 @@ contract OurToken is ERC20 {
     /// Mint tokens
     /// @param amount the number of tokens to mint
     /// @dev calls the ERC20's _mint function, which also transfer the minted tokens to the msg.sender
-    function mint(uint256 amount) external {
+    function mint(uint256 amount) external onlyOwner {
         _mint(msg.sender, amount);
     }
 }
